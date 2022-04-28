5efa6e7dcbc660e1d8ac18b6a37762f84279fe36
agira <imagirina@gmail.com>
2022-04-28 12:32:43 -0700

    Added Flask routes and crud functions

--- crud.py
+++ crud.py
@@ -2,6 +2,7 @@
 from model import db, User, Movie, Rating, connect_to_db
 
 
+# USERS
 def create_user(email, password):
     """Create and return a User object."""
 
@@ -9,7 +10,18 @@ def create_user(email, password):
 
     return user
 
+def get_users():
+    """Return a list of users."""
+    
+    return User.query.all()
 
+def get_user_details(user_id):
+    """Return particular user's details."""
+    
+    return User.query.get(user_id)
+
+
+# MOVIES
 def create_movie(title, overview, release_date, poster_path):
     """Create and return a Movie object."""
 
@@ -18,6 +30,17 @@ def create_movie(title, overview, release_date, poster_path):
     return movie
 
 
+def get_movies():
+    """Return a list of movies."""
+
+    return Movie.query.all()
+
+def get_movie_details(movie_id):
+    """Return particular movie's details."""
+
+    return Movie.query.get(movie_id)
+
+# RATINGS
 def create_rating(score, movie, user):
     """Create and return a Rating object."""
 
--- server.py
+++ server.py
@@ -1,17 +1,55 @@
 """Server for movie ratings app."""
 
-from flask import Flask
+from flask import Flask, render_template, request, session, redirect, flash
+from model import connect_to_db, db
+import crud
+from jinja2 import StrictUndefined
 
 app = Flask(__name__)
+app.secret_key = "dev"
+app.jinja_env.undefined = StrictUndefined
 
 
-# Replace this with routes and view functions!
+# Homepage Route
+@app.route('/')
+def homepage():
+    return render_template('homepage.html')
+
+
+# Movies route (list of movies)
+@app.route('/movies')
+def all_movies():
+    movies = crud.get_movies()
+
+    return render_template('movies.html', movies = movies)
+
+# Show details of particular movie
+@app.route('/movies/<movie_id>')
+def movie_details(movie_id):    
+    movie_details = crud.get_movie_details(movie_id)
+    
+    return render_template('movie_details.html', movie=movie_details)
+
+
+# Users route (list of users)
+@app.route('/users')
+def all_users():
+    users = crud.get_users()
+
+    return render_template('users.html', users = users)
+
+
+# Show details of particulat user
+@app.route('/users/<user_id>')
+def user_details(user_id):
+    user_details = crud.get_user_details(user_id)
+
+    return render_template("user_details.html", user=user_details)
+
 
 
 if __name__ == "__main__":
     # DebugToolbarExtension(app)
-    
-    # from model import connect_to_db#
-    # connect_to_db(app, "ratings")#
 
+    connect_to_db(app)
     app.run(host="0.0.0.0", debug=True)
